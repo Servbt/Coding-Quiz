80250d37ddd3b9bf7ceef6d92461eee5bf456eb9
Servbt <arianrezvani00@gmail.com>
2022-06-30 22:31:31 -0400

    added some WIP classes/id and some pseudo-code

---
+++ assets/JS/scripts.js
@@ -0,0 +1,9 @@
+
+// I want to make a quiz type game
+
+// i need a button that triggers the quiz and the timer
+    // for the button I need an event listener 
+    // once the button is clicked I have to hide the contents of the first page
+    // once they are hidden I have to display the new contents for the quiz
+
+
