9397b21cd48f6d71d3ba3af7b4e50dacc955747c
Servbt <arianrezvani00@gmail.com>
2022-07-01 00:42:40 -0400

    added an interval function, now I just need to display it

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -2,17 +2,38 @@
 // I want to make a quiz type game
 
 // i need a button that triggers the quiz and the timer
-    // for the button I need an event listener 
-    // once the button is clicked I have to hide the contents of the first page
+    // for the button I need an event listener ~
+    // once the button is clicked I have to hide the contents of the first page ~
     // once they are hidden I have to display the new contents for the quiz
     
+
+
+
     var firstPage = document.getElementById("main-pg");
     var startBtn = document.getElementById("first-btn");
+    var timerCnt = document.getElementById("#test")
 
-
-function quizStart (){
-    console.log("front button pressed");
+    function quizStart (){
+    
+        timerCnt.textContent.value =  "hello";
+    
     firstPage.setAttribute("style", "display: none");
+    console.log("hide quiz main page");   
+    
+    var timeLeft = 5;
+    var timer = setInterval(function() {
+        if (timeLeft > 1){
+            timeLeft--;
+        } else if (timeLeft === 1){
+            console.log("timer over");
+            clearInterval(timer);
+            textContent = "no more time left";
+        };
+        
+        console.log(timeLeft);
+    },1000);
+
+
 };
 
 
