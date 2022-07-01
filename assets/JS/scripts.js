9ee1587279ca9e1692522edd3f00b9bfb4cd5628
Servbt <arianrezvani00@gmail.com>
2022-06-30 23:36:45 -0400

    fixed some stylesheeting and added some more divs, also added button function to begin

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -5,5 +5,15 @@
     // for the button I need an event listener 
     // once the button is clicked I have to hide the contents of the first page
     // once they are hidden I have to display the new contents for the quiz
+    
+    var mainPage = document.getElementById("main-pg");
+    var startBtn = document.getElementById("first-btn");
+    var frontPg = document.getElementById("#main-pg");
+
+function quizStart (){
+    console.log("front button pressed");
+};
 
 
+
+startBtn.addEventListener("click", quizStart)
