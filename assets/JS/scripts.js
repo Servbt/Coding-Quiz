fee8847348dbed746dcfe0fdf849b11bc7564d5b
Servbt <arianrezvani00@gmail.com>
2022-07-03 22:00:48 -0400

    I made an entirely new array full of objects to reference from, also used an if statement to go through questions

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -8,9 +8,16 @@
         // I have to make an object with a header and 4 answer buttons ~
         // those buttons need event listeners to go to the next question ~
         // after a question is answered, right or wrong is displayed next as well
+            //need something to happen once I click an answer.. right or wrong
         // if wrong answer is given I need to tick the timer down
         // and finally i need to save the responses to score the user
 
+
+
+
+        // I have a list of questions... now to just put them into the screen
+            // made avariable that shows current placement
+            // need to put object properties on 
 
 
         var firstPage = document.getElementById("main-pg");
@@ -22,13 +29,20 @@
         var quesBtn3 = document.getElementById("answer3");
         var quesBtn4 = document.getElementById("answer4");
         var container = document.getElementById("btn-container");
-        
+
+
+
+        var currentQuestion;
+
+
         //traversing to select divider elements
         var quesHeader = document.querySelector("#question");
         var quesAns1 = document.querySelector("#answer1");
         var quesAns2 = document.querySelector("#answer2");
         var quesAns3 = document.querySelector("#answer3");
         var quesAns4 = document.querySelector("#answer4");
+
+        
 
         //buttons start off as hidden
         container.setAttribute("style", "display: none");
@@ -40,60 +54,93 @@
         
         
         // questions themselves
-        var question1 = {
+        var questionnaire = [
+        {
             question: "What is your favorite color?",
             answerA: "red",
             answerB: "blue",
             answerC: "green",
             answerD: "black",
-        };
-        var question2 = {
+        },
+        {  
             question: "What is your new favorite color....?",
             answerA: "white",
             answerB: "yellow",
             answerC: "grey",
             answerD: "pink",
-        };
-        var question3 = {
+        },
+        {
             question: "What?",
             answerA: "white",
             answerB: "yellow",
             answerC: "grey",
             answerD: "pink",
-        };
-        var question4 = {
+        },
+        {
             question: "quesrion4?",
             answerA: "whi123te",
             answerB: "yeasllow",
             answerC: "gwdqrey",
             answerD: "pinasdk",
-        };
-        var question5 = {
+        },
+        {
             question: "question5",
             answerA: "whasfdsite",
             answerB: "yelawfcaslow",
             answerC: "greafaxzy",
             answerD: "pinzxczxcdiek",
-        };
-        
+        }
+        ]
         
 
         
         function quizStart (){
             
+
+
             //once the button is pressed the buttons appear back
             container.setAttribute("style", "display: flex");
             firstPage.setAttribute("style", "display: none");
-            console.log("hide quiz main page");   
-            //first question initiated
-            quesHeader.innerHTML = question1.question;
-            quesAns1.innerHTML = question1.answerA;
-            quesAns2.innerHTML = question1.answerB;
-            quesAns3.innerHTML = question1.answerC;
-            quesAns4.innerHTML = question1.answerD;
+            console.log("hide quiz main page");
+
+                
+                var quesIndex = 0;
+
+
+                quesHeader.textContent = questionnaire[quesIndex].question;
+                quesAns1.textContent = questionnaire[quesIndex].answerA;
+                quesAns2.textContent = questionnaire[quesIndex].answerB;
+                quesAns3.textContent = questionnaire[quesIndex].answerC;
+                quesAns4.textContent = questionnaire[quesIndex].answerD;
+
+
+                 function next(){
+                     if (quesIndex  >= 0) {
+
+                         if (quesIndex === 5) {
+                             console.log("you are done!!");
+                        
+                             } 
+
+                         quesIndex++;
+                    quesHeader.textContent = questionnaire[quesIndex].question;
+                    quesAns1.textContent = questionnaire[quesIndex].answerA;
+                    quesAns2.textContent = questionnaire[quesIndex].answerB;
+                    quesAns3.textContent = questionnaire[quesIndex].answerC;
+                    quesAns4.textContent = questionnaire[quesIndex].answerD;
+
+                }
+            }
+
+
+            quesBtn1.addEventListener("click", next);
+            quesBtn2.addEventListener("click", next);
+            quesBtn3.addEventListener("click", next);
+            quesBtn4.addEventListener("click", next);
+
+
             
-            
-    
+        //function for timer itself
         var timer = setInterval(function() {
             if (timeLeft > 1){
                 timeLeft--;
@@ -106,63 +153,6 @@
             console.log(timeLeft);
         },1000);
         
-        // Question functions... might need to make this code more dry...
-        //2nd question
-        function nextQues1(){
-            quesHeader.innerHTML = question2.question;
-            quesAns1.innerHTML = question2.answerA;
-            quesAns2.innerHTML = question2.answerB;
-            quesAns3.innerHTML = question2.answerC;
-            quesAns4.innerHTML = question2.answerD;
-    
-                // 3rd question
-                function nextQues2(){
-                    quesHeader.innerHTML = question3.question;
-                    quesAns1.innerHTML = question3.answerA;
-                    quesAns2.innerHTML = question3.answerB;
-                    quesAns3.innerHTML = question3.answerC;
-                    quesAns4.innerHTML = question3.answerD;
-                        // 4th question
-                        function nextQues3(){
-                                quesHeader.innerHTML = question4.question;
-                                quesAns1.innerHTML = question4.answerA;
-                                quesAns2.innerHTML = question4.answerB;
-                                quesAns3.innerHTML = question4.answerC;
-                                quesAns4.innerHTML = question4.answerD;
-                                // last question
-                                function nextQues4(){
-                                        quesHeader.innerHTML = question5.question;
-                                        quesAns1.innerHTML = question5.answerA;
-                                        quesAns2.innerHTML = question5.answerB;
-                                        quesAns3.innerHTML = question5.answerC;
-                                        quesAns4.innerHTML = question5.answerD;
-                        };
-                    //event listeners for 5th question
-                    quesBtn1.addEventListener("click", nextQues4);
-                    quesBtn2.addEventListener("click",nextQues4);
-                    quesBtn3.addEventListener("click",nextQues4);
-                    quesBtn4.addEventListener("click",nextQues4);
-                };
-                //event listeners for 4th question
-                quesBtn1.addEventListener("click", nextQues3);
-                quesBtn2.addEventListener("click",nextQues3);
-                quesBtn3.addEventListener("click",nextQues3);
-                quesBtn4.addEventListener("click",nextQues3);
-            };
-            //event listeners for 3rd question
-            quesBtn1.addEventListener("click", nextQues2);
-            quesBtn2.addEventListener("click",nextQues2);
-            quesBtn3.addEventListener("click",nextQues2);
-            quesBtn4.addEventListener("click",nextQues2);
-        };
-        //event listeners for 2nd question
-        quesBtn1.addEventListener("click", nextQues1);
-        quesBtn2.addEventListener("click",nextQues1);
-        quesBtn3.addEventListener("click",nextQues1);
-        quesBtn4.addEventListener("click",nextQues1);
-    
-    
-    
     
     };
     
